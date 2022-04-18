0a8c7eca2eeedc682c84423d1d746b979d10d58c
1156054203 <1156054203@qq.com>
2022-04-18 15:20:20 +0800

    internal network pierce

--- run_pierced.sh
+++ run_pierced.sh
@@ -3,6 +3,7 @@
 stfdir=/sibcb2/bioinformatics2/chenyulong/software/pierced/linux
 ip=10.42.1.64
 port=8080
+name=chenyulong
 
 function usage {
     echo
@@ -12,12 +13,13 @@ function usage {
     echo "    the script is used to Mapping a local area network to a public network by pierced."
     echo "        -i/--ip ip -- ip address, [default: $ip]."
     echo "        -p/--port port -- port number, [default: $port]."
+    echo "        -n/--name name -- the prefix of output network name, [default: '$name']."
     echo "        -h/--help -- show help message."
     echo
     exit 0
 }
 
-ARG=$(getopt -q -o hi:p: --long help,ip:port: -n $0 -- $@)
+ARG=$(getopt -q -o hi:p:n: --long help,ip:port:name: -n $0 -- $@)
 
 eval set -- $ARG
 while [ -n "$1" ];do
@@ -26,6 +28,8 @@ case "$1" in
       ip=$2;shift 2;;
    -p|--port)
       port=$2;shift 2;;
+   -n|--name)
+      name=$2;shift 2;;
    -h|--help)
       help=true;shift 1;;
    *)
@@ -36,5 +40,5 @@ done
 if [[ $help == 'true' ]];then
    usage
 else
-   $stfdir/ding -config=$stfdir/ding.cfg -subdomain=chenyulong $ip:$port
+   $stfdir/ding -config=$stfdir/ding.cfg -subdomain=$name $ip:$port
 fi
